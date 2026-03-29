1419e79c67e5ed40260efa91be7e6acf76777e1c
Timilehin Adefioye <oadefioy@fandm.edu>
2026-03-29 17:30:09 -0400

    made layout for login need to add the full code next

--- app/src/main/java/edu/fandm/atalgatk/quiz2day/Login.java
+++ app/src/main/java/edu/fandm/atalgatk/quiz2day/Login.java
@@ -21,4 +21,47 @@ public class Login extends AppCompatActivity {
             return insets;
         });
     }
-}
+
+
+    private void signIn(String email, String password){
+        Task s = fba.signInWithEmailAndPassword(email, password);
+        s.addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(Task task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser user = fba.getCurrentUser();
+                    Toast.makeText(getApplicationContext(), "Login
+                            Successful!", Toast.LENGTH_LONG).show();
+                    updateUI(user);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Failed to
+                            login :(", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    private void registerNewUser(String email, String password){
+        Task<AuthResult> task = fba.createUserWithEmailAndPassword(email,
+                password);
+        task.addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(Task task) {
+                Log.d(TAG, "task: " + task);
+                if (task.isSuccessful()) {
+                    FirebaseUser user = fba.getCurrentUser(); // we're now
+                    logged in immediately!
+                            Toast.makeText(getApplicationContext(), "New User
+                                    Created!", Toast.LENGTH_LONG).show();
+                    updateUI(user);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Failed to
+                            create new user :(", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    // Display the user's UID
+    private void updateUI(FirebaseUser user){
+        TextView tv = (TextView) findViewById(R.id.fb_login_uid_tv);
+        tv.setText("Login Successful!\nUID: " + user.getUid());
+    }}
