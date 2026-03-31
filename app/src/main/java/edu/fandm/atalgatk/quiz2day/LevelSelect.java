5c57cbbcf2ab745d5a5d67921106845c09458ee1
awabnq <awabnq1@gmail.com>
2026-03-31 14:43:54 -0400

    Added Pages 1 and 3

---
+++ app/src/main/java/edu/fandm/atalgatk/quiz2day/LevelSelect.java
@@ -0,0 +1,53 @@
+package edu.fandm.atalgatk.quiz2day;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LevelSelect extends AppCompatActivity {
+
+    private ImageButton btnBack;
+    private Button btnLevel1, btnLevel2, btnLevel3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_level);
+
+        btnBack = findViewById(R.id.btnBack);
+        btnLevel1 = findViewById(R.id.btnLevel1);
+        btnLevel2 = findViewById(R.id.btnLevel2);
+        btnLevel3 = findViewById(R.id.btnLevel3);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btnLevel1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SelectLevelActivity.this, "Beginner Selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnLevel2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SelectLevelActivity.this, "Intermediate Selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnLevel3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SelectLevelActivity.this, "Advanced Selected", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
