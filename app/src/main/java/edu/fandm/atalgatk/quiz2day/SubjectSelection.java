e1400d9bfcc5c21c732ee6b76520bb8652afe729
Aastha Rijal <aastha@Aasthas-MacBook-Air.local>
2026-03-30 22:52:30 -0400

    Subject code

--- app/src/main/java/edu/fandm/atalgatk/quiz2day/SubjectSelection.java
+++ app/src/main/java/edu/fandm/atalgatk/quiz2day/SubjectSelection.java
@@ -1,6 +1,9 @@
 package edu.fandm.atalgatk.quiz2day;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,30 @@ public class SubjectSelection extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //setting all my buttons
+        Button english = findViewById(R.id.english_button);
+        english.setOnClickListener(v-> openQuestion("English"));
+
+        Button math = findViewById(R.id.math_button);
+        math.setOnClickListener(v->openQuestion("Math"));
+
+        Button science = findViewById(R.id.science_button);
+        science.setOnClickListener(v->openQuestion("Science"));
+
+        Button ss = findViewById(R.id.ss_button);
+        ss.setOnClickListener(v->openQuestion("Social Studies"));
+
+        ImageButton back = findViewById(R.id.back_button);
+        back.setOnClickListener(v->finish());
     }
+
+
+    //have to change
+    private void openQuestion(String subject){
+        Intent i = new Intent(this, Questions.class);
+        i.putExtra("subject", subject);
+        startActivity(i);
+    }
+
 }
